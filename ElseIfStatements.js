53d926feac6a9bb011f545637469f7243ecae973
Lucy Ly <lucyly.student@careerdevs.com>
2019-04-05 15:16:33 +0000

    updated

--- ElseIfStatements.js
+++ ElseIfStatements.js
@@ -1,8 +1,7 @@
 // Else Statement
 function testElse(val) {
   var result = "";
-  // Only change code below this line
-  
+
   if (val > 5) {
     result = "Bigger than 5";
   
@@ -10,11 +9,9 @@ function testElse(val) {
     result = "5 or Smaller";
   }
   
-  // Only change code above this line
   return result;
 }
 
-// Change this value to test
 testElse(4);
 
 // When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
@@ -34,7 +31,6 @@ function testElseIf(val) {
   }
 }
 
-// Change this value to test
 testElseIf(7);
 
 // If you have multiple conditions that need to be addressed, you can chain if statements together with else if statements.
